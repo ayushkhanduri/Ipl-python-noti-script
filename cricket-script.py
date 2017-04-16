52a715ab8fd23b51034ad514669413eaad3eb5fd
ayushkhanduri khanduri <ayushkhanduri@gmail.com>
2017-04-16 20:17:47 +0530

    Getting the latest match score bug

--- cricket-script.py
+++ cricket-script.py
@@ -2,23 +2,31 @@ import requests
 import notify2
 from time import sleep
 from bs4 import BeautifulSoup
+
 def displayNotification(data):
 	notify2.init("AnyNotifier");
 	matchId = checkMatchStatus(data)
 	score = data[matchId].text if matchId!=-1 else "No favourite teams playing"
-	notification = notify2.Notification(score)
+	notification = notify2.Notification("Score: ", score)
 	notification.show()
 
 #finds if your favourite team has a match going on
 def checkMatchStatus(data):
 	allTeams = ["Delhi DareDevils" , "Gujarat Lions","Kings XI Punjab","Kolkata Knight Riders","Mumbai Indians","Rising Pune Supergiant","Royal Challengers Bangalore","Sunrisers Hyderabad"]
-	found =-1
+	allMatchesToday = []
 	for index1 in range(len(data)):
 		for index2 in range(len(allTeams)):
 			if data[index1].text.find(allTeams[index2])!=-1:
-				found = index1
+				allMatchesToday.append(index1)	
 				break
-		if found !=-1:
+	return currentMatchGoing(data,allMatchesToday);
+
+def currentMatchGoing(data,allMatchesToday):
+	found = -1
+	print(allMatchesToday)
+	for index in reversed(allMatchesToday):
+		if data[index].text.find("*")!=-1:
+			found = index
 			break
 	return found;	
 	
